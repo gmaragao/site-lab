26b6a99f182745e6c4ae425ff8beefbff910a939
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-04-29 16:16:52 -0300

    Changing footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,16 +26,20 @@ const PageLayout = props => {
             </Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: "0 50px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>{__dirname}</Breadcrumb.Item>
-          </Breadcrumb>
+        <Content style={{ padding: "0 50px", marginTop: 10 }}>
           <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
             {props.children}
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
+          <p>Handcrafted with &hearts; by Lab Ventures</p>
+          <p>
+            Reach us at:{" "}
+            <a href="https://www.google.com.br/maps/place/Av.+Pl%C3%ADnio+Kroeff,+1715+-+Rubem+Berta,+Porto+Alegre+-+RS,+91150-170/@-29.9968535,-51.1156693,17z/data=!3m1!4b1!4m5!3m4!1s0x9519768b9a75caa7:0x63c95e432595a41a!8m2!3d-29.9968582!4d-51.1134753">
+              Av. Plínio Kroeff, 1715.
+            </a>{" "}
+            Porto Alegre/RS - Brazil{" "}
+          </p>
         </Footer>
       </Layout>
     </div>
